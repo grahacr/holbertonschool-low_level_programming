ff489fa08125d2aff559908e9742c5e4731c692f
Courtney Graham <7194@holbertonstudents.com>
2023-09-22 14:45:36 -0700

    print whether the number stored in variable n is positive or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * specifies return point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (int>0; int==0; int<0; "%d is positive"; "%d is zero"; "%d is negative\n");
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This repo is for learning C, the variables if, else, and while
