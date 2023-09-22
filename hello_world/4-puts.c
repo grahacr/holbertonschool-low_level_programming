3b9c5d631a5fa54f510538ec24667fab72ee4021
Courtney Graham <7194@holbertonstudents.com>
2023-09-22 09:31:54 -0700

    fix for betty checker

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/* Script should print simple line of text and compile correctly */
+/* Script should enter in main and return */
+
 int main(void)
-/* main is the entry point */
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
