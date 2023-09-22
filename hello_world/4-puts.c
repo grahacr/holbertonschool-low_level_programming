c891934930d4e94c7ff7be9a40dd56a2456e7e03
Courtney Graham <7194@holbertonstudents.com>
2023-09-22 09:28:17 -0700

    fix main comment

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /* Script should print simple line of text and compile correctly */
 int main(void)
+/* main is the entry point */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
