68284244f2f58306474a15297b83bcfc4cab980f
Courtney Graham <7194@holbertonstudents.com>
2023-09-22 10:08:17 -0700

    program prints sizes of various types on computer it is compiled and run on

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * 
+ * specifies return point
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of char: %zu bytes\n", sizeof(char));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+return (0)
+}
