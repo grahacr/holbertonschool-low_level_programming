734082b1d3c18718b11eee4a02b5db21c863ace8
Courtney Graham <7194@holbertonstudents.com>
2023-09-22 09:24:34 -0700

    fix characters for betty checker

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /* Script should print simple line of text and compile correctly */
-int main()
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
