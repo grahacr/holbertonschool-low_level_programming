cce8283e3290b98a8f731073b61f7e028bafe1f5
Courtney Graham <7194@holbertonstudents.com>
2023-09-22 09:19:05 -0700

    c program that prints a specific line and is compiled

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#!/bin/bash
 #include <stdio.h>
-int main() {
+/* Script should print simple line of text and compile correctly */
+int main()
+{
 	puts("Programming is like building a multilingual puzzle");
 	return 0;
 }
