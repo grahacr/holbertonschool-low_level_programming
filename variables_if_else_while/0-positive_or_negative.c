38053418f6ab9be7883e7b859268515b2eb1a473
Courtney Graham <7194@holbertonstudents.com>
2023-09-23 10:34:53 -0700

    tryihng something new

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -17,10 +17,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	{
 	if (n > 0)
-	{printf("%d is positive\n" n);
+	{printf("$n is positive\n");
 	}
 	else if (n == 0)
-	{printf("%d is zero\n" n);
+	{printf($n"is zero\n");
 	}
 	else (n < 0)
 	{printf("%d is negative\n" n);
