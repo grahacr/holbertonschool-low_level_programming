526144ec7616590da34a9f44e7188ae5377a0dcb
Courtney Graham <7194@holbertonstudents.com>
2023-09-23 09:33:23 -0700

    fix indentation

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -15,11 +15,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n>0);
-	printf("%d is positive", n;);
-	else(n==0);
-	printf("%d is zero", n;);
-	else(n<0);
-	printf("%d is negative", n;);
-	return (0);
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+	else(n < 0)
+	{
+		printf("%d is negative", n);
+	}
+return (0);
 }
