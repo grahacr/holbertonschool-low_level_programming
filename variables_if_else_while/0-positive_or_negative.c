f6c2464e576c79c10910e0a7e51e4ab14e539376
Courtney Graham <7194@holbertonstudents.com>
2023-09-23 09:51:46 -0700

    eliminate brackets

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -18,14 +18,10 @@ int main(void)
 	{
 	if (n > 0)
 		printf("%d is positive\n", n);
-	}
-	{
 	else if (n == 0)
 		printf("%d is zero\n", n);
-	}
-	{
 	else(n < 0)
-	}
 		printf("%d is negative\n", n);
+	}
 return (0);
 }
