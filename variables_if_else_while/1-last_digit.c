bdcd3ac3505805209fe51329cf33f5356f1c8ad8
Courtney Graham <7194@holbertonstudents.com>
2023-09-23 12:11:01 -0700

    fix small error

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 	else if (n == 0)
 	{printf("Last digit of n is %d and is 0", n % 10);
 	}
-	else if
+	else
 	{printf("Last digit of n is %d and is less than 6 and not 0", n % 10);
 	}
 }
