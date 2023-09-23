c073beb4d197efab3b08906e75b9894a59eaf8b6
Courtney Graham <7194@holbertonstudents.com>
2023-09-23 10:27:12 -0700

    bettyyyyyy

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,12 +16,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	{
-	if (n > 0) do
-		printf("%d is positive\n", n);
-	else if (n == 0) do
-		printf("%d is zero\n", n);
-	else(n < 0) do
-		printf("%d is negative\n", n);
+	if (n > 0)
+	{printf("%d is positive\n" n);
 	}
+	else if (n == 0)
+	{printf("%d is zero\n" n);
+	}
+	else (n < 0)
+	{printf("%d is negative\n" n);
+	}
+}
 return (0);
 }
