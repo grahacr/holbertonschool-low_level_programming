7d663dc8e0b9a1d65632f03eae707aeb5ae9b264
Courtney Graham <7194@holbertonstudents.com>
2023-09-24 13:46:28 -0700

    fixed description and declaration

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * specifies return point
+ * Return: 0
+ */
 int main(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
 		putchar(letter);
