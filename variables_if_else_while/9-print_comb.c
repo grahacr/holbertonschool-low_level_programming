f6b4f06f28e4f55045eb3327073ec52082c3ab6e
Courtney Graham <7194@holbertonstudents.com>
2023-09-25 10:43:28 -0700

    utilize if statement to fix output of last number 9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,11 +9,15 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	if (i == 0; i < 9; i++)
 	{
 		putchar('0' + i);
 		putchar(',');
 		putchar(' ');
+	}
+	else if (i = 9)
+	{
+		putchar('0' + i);
 	}
 return (0);
 }
