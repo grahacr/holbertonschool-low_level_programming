36d520554fd5b48e55a6035877c31d0363d5027f
Courtney Graham <7194@holbertonstudents.com>
2023-09-25 10:12:05 -0700

    print all combinations of single digit numbers in ascending order followed by space

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * specifies return point
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	{
+	for (i = 10; i <= 9; i++)
+	{
+	putchar('i')
+	}
+	}
+	putchar(',\n');
+return (0);
+}
