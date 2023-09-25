3de3199e4f8f26d31be4a587af6d9630ea707140
Courtney Graham <7194@holbertonstudents.com>
2023-09-25 12:04:17 -0700

    change syntax to fix output

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,17 +10,14 @@ int main(void)
 	int i;
 
 	{
-	for (i = 0; i < 10; i != 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar('0', i);
+	}
+	if (i != '9')
+	{
 		putchar(',');
 		putchar(' ');
-	}
-	{
-		if (i == '9')
-		{
-			putchar('0' + i);
-		}
 	}
 	}
 return (0);
