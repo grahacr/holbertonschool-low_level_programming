c1914b62b560b11276ddfc356b956913a79baf47
Courtney Graham <7194@holbertonstudents.com>
2023-09-26 20:59:17 -0700

    Fix brackets again

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -4,17 +4,19 @@
  * Return: 0
  */
 void print_alphabet_x10(void);
+{
 	int i;
 
 	for (i = 0; i <= 10; i++)
 	{
-		char letter = 'a';
+	char letter = 'a';
 
-		while (letter <= 'z')
-		{
-			_putchar(letter);
-			letter++;
-		}
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
 		_putchar('\n');
 	}
+}
 return(0)
