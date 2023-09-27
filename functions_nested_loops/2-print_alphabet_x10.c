384e7eff66b8cc7114320bd4f92320dff6fcbd61
Courtney Graham <7194@holbertonstudents.com>
2023-09-26 20:56:14 -0700

    write function that prints entire lowercase alphabet 10 times

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alpha ten times
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+		_putchar('\n');
+	}
+}
+return(0)
