71af888a05516e14fafa812bc29a50cedcb1a3d8
Courtney Graham <7194@holbertonstudents.com>
2023-09-27 13:28:26 -0700

    fix comments

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
- *
+ * _isdigit - checks for digits zero through nine
+ * @c: character to check
+ * Return: 1 if it is a digit zero through nine, 0 otherwise
  *
  */
 int _isdigit(int c)
