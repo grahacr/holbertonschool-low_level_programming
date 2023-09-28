f6742bdf6395f7a60dd7272b669df418cd25926e
Courtney Graham <7194@holbertonstudents.com>
2023-09-27 21:38:17 -0700

    fix brackets

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,13 @@ void print_most_numbers(void)
 {
 	char n = '0';
 
+	while (n >= 0 && n <= 9)
 	{
-		while (n >= 0 && n <= 9)
+		if (n != 2 && n != 4)
 		{
-			if (n != 2 && n != 4)
+			_putchar(n);
 		}
+		n++
 	}
-	_putchar (n++);
 	_putchar ('\n');
 }
