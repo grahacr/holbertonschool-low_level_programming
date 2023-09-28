fb69891941894f8551ef66b66e2152633bde45bf
Courtney Graham <7194@holbertonstudents.com>
2023-09-27 21:53:36 -0700

    function print 10 times the numbers zero to fourteen

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - print most numbers except two and four
+ * Return: void on success
+ */
+void more_numbers(void)
+{
+	char n;
+
+	char i <= 10;
+
+	while (n >= '0' && n <= '10')
+	{
+		if (i <= 10)
+		{
+			_putchar(i++);
+		}
+		_putchar(n);
+	}
+	_putchar ('\n');
+}
