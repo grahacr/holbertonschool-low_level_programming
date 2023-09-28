a212f7817f14584cc55b069d136c8066c5d0e607
Courtney Graham <7194@holbertonstudents.com>
2023-09-27 21:41:20 -0700

    fix output to print numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,9 @@ void print_most_numbers(void)
 {
 	char n = '0';
 
-	while (n >= 0 && n <= 9)
+	while (n >= '0' && n <= '9')
 	{
-		if (n != 2 && n != 4)
+		if (n != '2' && n != '4')
 		{
 			_putchar(n);
 		}
