9f53d02383a4da6f10db17f66b93c18109ae981a
Courtney Graham <7194@holbertonstudents.com>
2023-09-27 21:28:37 -0700

    fix nested loops

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,15 @@
  */
 void print_most_numbers(void)
 {
-	char n;
+	char n = '0';
 
 	while (n >= 0 && n <= 9)
 	{
 		if (n != 2 && n != 4)
 		{
-			_putchar (n + 0);
+			_putchar (n);
 		}
-	_putchar ('\n');
+		n++;
 	}
-return(void);
+	_putchar ('\n');
 }
