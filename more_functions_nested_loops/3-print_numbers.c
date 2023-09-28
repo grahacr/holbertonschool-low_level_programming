b607d2e3d94b17a61e758fc9b3f529e85d5c794f
Courtney Graham <7194@holbertonstudents.com>
2023-09-27 19:29:37 -0700

    function prints the numbers 0 to 0 followed by new line

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers zero through nine
+ * @n: number to read
+ *
+ *
+ */
+void print_numbers(int n)
+{
+	if (n >= 0 && n <= 9)
+		_putchar("%d");
+	_putchar("\n");
+}
