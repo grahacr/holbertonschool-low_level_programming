88c18ec87ce9201a5f5c8914cb957a798ba93917
Courtney Graham <7194@holbertonstudents.com>
2023-09-28 20:48:55 -0700

    print squares

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,14 +3,16 @@
  * print_line - print line ending in new line
  * @n: integer to check
  * Return: 0 on success
+ *
  */
 void print_line(int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
 	{
-		_putchar('_');
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
