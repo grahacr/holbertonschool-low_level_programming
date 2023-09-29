3e80602d0baa2f0ed8039b228a8acc57d486a768
Courtney Graham <7194@holbertonstudents.com>
2023-09-29 10:02:03 -0700

    print triangle using hashtags

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_triangle - print triangle shape using #
+ * @size: size of triangle
+ * Return: 0 on success
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
