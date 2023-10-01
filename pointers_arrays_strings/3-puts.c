b02b86766fab71a0392409f939f8da073784f8d6
Courtney Graham <7194@holbertonstudents.com>
2023-10-01 14:21:21 -0700

    minor fixes

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- *
+ * _puts - print string
+ * @str: string of characters to print
  *
  */
 void _puts(char *str)
 {
 	int i;
-	for (i = 0; str[i] != '\0'; i++)
+
+	for (i >= 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
