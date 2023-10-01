ccda607ef2258ee3a27785f59d3f3bec20912499
Courtney Graham <7194@holbertonstudents.com>
2023-10-01 13:37:19 -0700

    simplify simplify simplify

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,10 @@
 /**
  * reset_to_98 - reset value pointed to
  * @n: random integer value to be changed
- * Return: 0 on success
+ * Return: 0 on succesis
  */
 void reset_to_98(int *n)
 {
-	int n;
-	int *p;
-
 	n = 5;
-	p = &n;
 	*p = 98;
-	return (0);
 }
