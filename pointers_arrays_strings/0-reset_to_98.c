cea2a68316be12c56a1bf9b264b2d75a36939daa
Courtney Graham <7194@holbertonstudents.com>
2023-10-01 13:32:55 -0700

    back to the original

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,15 @@
 /**
  * reset_to_98 - reset value pointed to
  * @n: random integer value to be changed
+ * Return: 0 on success
  */
 void reset_to_98(int *n)
 {
-	int p;
-	int *n;
+	int n;
+	int *p;
 
-	n = &p;
+	n = 5;
+	p = &n;
 	*p = 98;
+	return (0);
 }
