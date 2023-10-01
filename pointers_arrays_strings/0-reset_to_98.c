0afc805872b3294ed118fcbe557218c9cd83e510
Courtney Graham <7194@holbertonstudents.com>
2023-10-01 13:20:06 -0700

    function takes pointer to int as parameter and updates value

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - reset value pointed to
+ * @n: random integer value to be changed
+ */
+void reset_to_98(int *n);
+{
+	int n;
+	int *p;
+
+	n = 402;
+	p = &n;
+	*p = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+We will practice pointers, arrays, and strings in this project.
