5aaea55af7dbd5b5119ed2504eaf702723530e02
Courtney Graham <7194@holbertonstudents.com>
2023-10-01 15:00:34 -0700

    fix syntax

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
- *
+ * print_rev - print reverse of characters in string
+ * @s: string of characters
  */
 void print_rev(char *s)
 {
-	int i;
-	
-	while(s[i] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for (i = s - 1; i >= 0; i--)
+	for (s = s - 1; s >= 0; s--)
 	{
 		_putchar(s[i]);
 	}
