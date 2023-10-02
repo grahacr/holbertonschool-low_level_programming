7ff07bde5bce2ad48491116bacb0b35d994c0234
Courtney Graham <7194@holbertonstudents.com>
2023-10-02 08:56:41 -0700

    fix declarations

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,13 @@
 void rev_string(char *s)
 {
 	int length = 0;
+	int start = 0;
+	int end = length - 1;
 
 	while (s[length] != '0')
 	{
 		length++;
 	}
-	int start = 0;
-	int end = length - 1;
-
 	while (start < end)
 	{
 		char temp = s[start];
