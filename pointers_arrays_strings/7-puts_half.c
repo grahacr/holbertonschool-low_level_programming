7e602822e1e15535177cfaf83e158d4b332d914f
Courtney Graham <7194@holbertonstudents.com>
2023-10-02 12:10:26 -0700

    print half of character string only

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+void puts_half(char *str);
+{
+	int length = 0;
+	int start;
+
+	while (str[length] != '\0')
+	{
+		length++
+	}
+	start = length / 2;
+	while (str[start] != '0')
+	{
+		_putchar(str[start]);
+		start++;
+	}
+	_putchar('\n');
+}
