d951bb8789a21f932c6c3d92d26bc6474624df29
Courtney Graham <7194@holbertonstudents.com>
2023-10-05 13:53:54 -0700

    concatenate two strings using n bytes from src string

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *_strncat - concatenated string
+ * @dest: destination string
+ * @src: source string
+ * @n: integer to take and not surpass
+ * Return: *_strncat - return pointer
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *_strncat = dest;
+
+	while (*_strncat != '\0')
+	{
+		_strncat++;
+	}
+	while (*src != '\0' && n > 0)
+	{
+		*_strncat = *src;
+		_strncat++;
+		src++;
+		n--;
+	}
+	*_strncat = '\0';
+}
+
