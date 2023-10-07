5424702d9a7b0b878a030e2823a3ea676569b6df
Courtney Graham <7194@holbertonstudents.com>
2023-10-06 17:11:08 -0700

    copy string similar to strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strncpy - function to copy strings
+ * @dest: destination string
+ * @src: source string
+ * @n: integer to take
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	*dest = '\0';
+	return (dest);
+}
