a6e5c692131918a0f9d521c36935e8b7da89ff5e
Courtney Graham <7194@holbertonstudents.com>
2023-10-08 15:05:55 -0700

    fix output

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -8,13 +8,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i = 0;
+	int i;
 
 	for (i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	for (i < n)
+	for (; i < n)
 	{
 		dest[i] = '\0';
 	}
