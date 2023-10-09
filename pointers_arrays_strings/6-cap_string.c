80e4803806b1951be16e50d4f868576bead7a55f
Courtney Graham <7194@holbertonstudents.com>
2023-10-09 11:40:24 -0700

    fix output

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,12 +8,11 @@
 char *cap_string(char *str)
 {
 	char sep[13] = {'"', ' ', '{', '}', '.', ';', ',', '!', '(', ')', '\n', '\t'};
+	int isSep = 1;
+	int i;
 
 	while (*str)
 	{
-		int isSep = 1;
-		int i;
-
 		for (i = 0; sep[i]; i++)
 		{
 			if (*str == sep[i])
