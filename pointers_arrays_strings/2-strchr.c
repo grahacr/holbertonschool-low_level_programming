e7a6f65f7b350b1bc00f014a9f7bad241733233e
Courtney Graham <7194@holbertonstudents.com>
2023-10-09 20:08:37 -0700

    search for character in string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - function to check for character in string
+ * @s: string to check
+ * @c: character to check for
+ * Return: NULL if not character
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s[i]);
+		}
+		i++;
+	}
+	return (NULL);
+}
