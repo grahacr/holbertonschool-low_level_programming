5fe95a7870de5245c6107e908854daca5e5b1f68
Courtney Graham <7194@holbertonstudents.com>
2023-10-09 21:39:37 -0700

    get length of a prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * _strspn - get length of prefix substring
+ * @s: character array segment
+ * @accept: char string to search
+ * Return: count
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+
+	while (*s != '0')
+	{
+		char *ptr = accept;
+
+		while (*ptr != '0')
+		{
+			if (*s == *ptr)
+			{
+				break;
+			}
+		ptr++;
+	}
+	if (*ptr == '\0')
+	{
+		return (count);
+	}
+	count++;
+	s++;
+	}
+	return (count);
+}
