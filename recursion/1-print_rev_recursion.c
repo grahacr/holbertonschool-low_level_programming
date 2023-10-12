4312e018c5f6741237301c9661fc08db21557a55
Courtney Graham <7194@holbertonstudents.com>
2023-10-11 19:57:02 -0700

    fix output of new line

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -7,9 +7,15 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
 	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	if (*s != ' ')
+	{
+		_putchar(*s);
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
