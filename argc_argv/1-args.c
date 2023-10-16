d5c536faa32b1dee05ac164ab2b12ae696747c1a
Courtney Graham <7194@holbertonstudents.com>
2023-10-16 14:21:46 -0700

    program print number of args passed to it

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,11 +1,19 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * main - main program
  * @argc: number of arguments
  * Return: 0 on success
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
-	_putchar("%d\n", argc);
+	if (argc > 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		printf("0\n");
+	}
 	return (0);
 }
