e4c8bbe8b786889506cbf3b7863f51325e545182
Courtney Graham <7194@holbertonstudents.com>
2023-10-16 14:59:58 -0700

    multiply two numbers passed into main

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -23,9 +23,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = multiply(num1, num2);
-	printf("%d\n", result);
-	return (0);
+	else
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		int result = multiply(num1, num2);
+
+		printf("%d\n", result);
+		return (0);
+	}
 }
