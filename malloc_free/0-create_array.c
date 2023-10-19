fba67108150b16b9d5bbc7f3576c5c5f140042d6
Courtney Graham <7194@holbertonstudents.com>
2023-10-19 10:54:54 -0700

    functions_nested_loops

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,30 +3,30 @@
 #include <stdlib.h>
 /**
  * create_array - create array of chars using malloc
- * @char: specific char to create array
- * @int: integer to create array
- * Return: array on success
+ * @c: specific char to initialize array
+ * @size: integer to create size of array
+ * Return: pointer to the array on success or NULL on failure
  */
 
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i = 0;
+	unsigned int i;
+	char *array;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
 
-	char *array = (char *)malloc(size * sizeof(char));
+	array = malloc(size * sizeof(char));
 
 	if (array == NULL)
 	{
 		return (NULL);
 	}
-	for (i; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
 	return (array);
-	free(create_array);
 }
