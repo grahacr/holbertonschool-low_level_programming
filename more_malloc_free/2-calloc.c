3fe9016dac60d1611d8f72abb8016d2c46656953
Courtney Graham <7194@holbertonstudents.com>
2023-10-23 11:59:10 -0700

    allocate memory for array using malloc and sets memory values to 0

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "0-memset.c"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _calloc - allocate memory and set value to 0
+ * @nmemb - number of elements
+ * @size = size of elements
+ * Return: calloc on success, NULL on failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int index;
+	char *calloc;
+
+	if nmemb = 0;
+	return (NULL);
+	if size = 0;
+	return (NULL);
+
+	calloc = malloc(nmemb * size);
+	if (calloc)
+	{
+		_memset(calloc, '\0', nmemb * size);
+		return (calloc);
+	}
+	return (NULL);
+}
