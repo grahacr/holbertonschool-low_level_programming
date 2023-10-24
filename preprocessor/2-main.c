8761654f3984cde4395f298a45b6a611a70ee2f8
Courtney Graham <7194@holbertonstudents.com>
2023-10-23 21:18:39 -0700

    fix errors

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *
- */
-#ifndef MAIN_H
-#define MAIN_H
 #define __FILE__
 {
-	printf("%s\n", __FILE__);
+	printf("%s\n", __FILE__)
 	return (0);
 }
-#endif
