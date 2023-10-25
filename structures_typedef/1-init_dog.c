8fc6739b751eb509d8b2050181aebd6e631dddc3
Courtney Graham <7194@holbertonstudents.com>
2023-10-25 15:18:11 -0700

    fix compilation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,11 +11,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog);
-	if (d == NULL)
-	return (NULL);
-	dog->name = name;
-	dog->age = age;
-	dog->owner = owner;
-	return(dog);
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
