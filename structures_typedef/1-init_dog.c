e07a2b4842f6e501663bd28ba5c3c82ce77fa7cb
Courtney Graham <7194@holbertonstudents.com>
2023-10-25 15:27:19 -0700

    fix compilation

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -3,14 +3,17 @@
 #include <stdio.h>
 /**
  * init_dog - initialize variable of type struct dog
- * @d: pointer to struc dog
+ * @d: pointer to struct dog
  * @name: name of dog to initialize
  * @age: age to initialize
  * @owner: owner to initialize
- * Return: dog on success
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+	{
+		return;
+	}
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
