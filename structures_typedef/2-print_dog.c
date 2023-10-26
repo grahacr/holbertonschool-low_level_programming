35eb1df3c92a656743a61452148e79d15c546649
Courtney Graham <7194@holbertonstudents.com>
2023-10-26 12:19:58 -0700

    print struct dog elements

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
+/**
+ * print_dog - print elements of struct dug
+ * @d: pointer to struct dog
+ */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
@@ -8,20 +12,20 @@ void print_dog(struct dog *d)
 		return;
 	}
 	if (d->name == NULL)
-		{
-			printf("Name: (nil)\n");
-		}
-		else
-		{
-			printf("Name: %s\n", d->name);
-		}
-		printf("Age: %f\n", d->age);
-		if (d->owner == NULL)
-		{
-			printf("Owner: (nil)\n");
-		}
-		else
-		{
-			printf("Owner: %s\n", d->owner);
-		}
+	{
+		printf("Name: (nil)\n");
+	}
+	else
+	{
+		printf("Name: %s\n", d->name);
+	}
+	printf("Age: %f\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	else
+	{
+		printf("Owner: %s\n", d->owner);
+	}
 }
