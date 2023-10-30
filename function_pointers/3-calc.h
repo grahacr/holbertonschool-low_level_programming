6c7ca8d17be7e55679f4adc851ff119405400f62
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 14:05:37 -0700

    function_pointers

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,5 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
+#include <stdio.h>
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,6 +1,7 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * get_op_func - select correct function to perform operation
  * @s: operator passed as argument to program
@@ -8,8 +9,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] =
-	{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
