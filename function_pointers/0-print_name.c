bdfbaf0424655f4b3b5171483a56655592f0ad6e
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 10:54:33 -0700

    betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
+ * print_name - print name in character array
+ * @name: name input
+ * @f: function pointer to character array
  */
 void print_name(char *name, void (*f)(char *))
 {
