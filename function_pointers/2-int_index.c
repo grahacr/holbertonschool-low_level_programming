1bba96c9cfed1fd0bf135f4339a346fff18a6300
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 12:10:55 -0700

    fix null case

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -9,16 +9,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (size <= 0 || cmp == NULL || array == NULL)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i] != 0))
 		{
 			return (i);
 		}
-		else
-		{
-			return (-1);
-		}
 	}
-	return (0);
 }
