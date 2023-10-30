79304a616069689b4dda0a8ad19e97579220e89c
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 13:41:35 -0700

    function_pointers

---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <strings.h>
+#include <stdlib.h>
+/**
+ * main - main method
+ * @argc: number of arguments
+ * @argv: arguments passed
+ * Return: 0 on success, -1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	int first;
+	int second;
+	char *op;
+	int (*result)(int, int);
+
+	if (argc <= 1)
+	{
+		return (-1);
+	}
+	first = atoi(argv[1]);
+	second = atoi(argv[3]);
+	operator = argv[2];
+	result = get_op_func(op);
+	if (result != NULL)
+	{
+		printf("%d\n", result(first, second));
+	}
+	return (0);
+}
