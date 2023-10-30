a1d7a14793330ccae83fa625d0c773d9f9aa0652
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 12:19:21 -0700

    betty

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * int_index - search array for integers 
+ * int_index - search array for integers
  * @array: array to search
  * @size: size of array
  * @cmp: function to compare integers
