10f95f7b5ff4fbfeb71a603a89ddffb73ec7068f
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 14:18:08 -0700

    function_pointers/1-array_iterator.c

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -9,7 +9,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
+	struct ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -17,7 +17,7 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
 	while (ops[i].op != NULL)
 	{
