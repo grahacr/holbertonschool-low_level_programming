599adbc8b5e465d437ed9511c9734406189fc959
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 20:12:58 -0700

    fix betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -3,13 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
- *
+ * print_numbers - print numbers given
+ * @separator: pointer to character to print between numbers
+ * @n: unsigned integer representing number of integers to be printed
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
