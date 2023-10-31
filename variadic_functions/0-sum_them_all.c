340fd68f7856377e95eba5409b53c899e6a0734a
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 19:21:05 -0700

    fix compilation

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,13 +9,15 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	if (n == 0)
+	{
+		return (0);
+	}
 	va_list args;
 	va_start(args, n);
-	int i;
+	const unsigned int i;
 	int sum = 0;
 
-	if (n == 0)
-		return (0);
 	for (i = 0; i < n; i++)
 	{
 		int value = va_arg(args, int);
