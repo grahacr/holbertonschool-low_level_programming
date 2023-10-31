fb98ab9fed4584c82651da1327c7734fd1fc1d6e
Courtney Graham <7194@holbertonstudents.com>
2023-10-30 20:41:22 -0700

    print nil if string is empty

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -11,8 +11,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
-	va_start(args, n);
 
+	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(args, char *);
@@ -20,10 +20,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str != NULL)
 		{
 			printf("%s", str);
-			if (separator != NULL && i < n - 1)
-			{
-				printf("%s", separator);
-			}
+		}
+		else
+		{
+			printf("(nil)");
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
 		}
 	}
 	printf("\n");
