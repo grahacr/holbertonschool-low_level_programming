6d1596ce366931c587534e3e95d75bd6a5ec2f93
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 15:41:47 -0800

    function_pointers

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,11 +10,22 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *current = malloc(sizeof(list_t));
 
-	while (current != NULL)
+	if (current == NULL)
 	{
-		*head -> next = current;
-		current = head;
+		return (NULL);
+	}
+	current->str = strdup(str);
+	current->len = strlen(str);
+
+	if (*head == NULL)
+	{
+		current->next = NULL;
+		*head = current;
+	}
+	else
+	{
+		current->next = *head;
+		*head = current;
 	}
 	return (current);
 }
-
