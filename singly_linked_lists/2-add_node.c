8ad2a556d990c351d6913d2d8524af775d0c6f9f
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 15:21:03 -0800

    function_pointers

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *current = malloc(sizeof(list_t));
+	size_t count;
+
+	while (current != NULL)
+	{
+		head -> next = current;
+		current = head;
+	}
+	return (count);
+}
+
