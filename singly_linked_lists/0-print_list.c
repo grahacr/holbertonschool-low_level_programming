6efad24ce70eea848e9b5199e4c1ba9d9ad37c83
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 13:22:44 -0800

    function_pointers

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -12,12 +12,12 @@ size_t print_list(const list_t *h)
 		if (current->str != NULL)
 		{
 			printf("[%lu] %s\n", current->len, current->str);
+			else
+			{
+				printf("[%lu] (nil)\n", current->len);
+			}
 			current = current->next;
 			count++;
-		}
-		else
-		{
-			printf("[%lu] (nil)\n", current->len);
 		}
 	}
 	return(count);
