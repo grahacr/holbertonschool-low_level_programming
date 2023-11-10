00c2f326224faddc6cb612b2e0fb06c52d88d372
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 12:42:07 -0800

    print all elements of linked list

---
+++ singly_listed_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+size_t print_list(const list_t *h)
+{
+	list_t *head;
+	list_t *new;
+	size_t n;
+
+	head = malloc(sizeof(list_t));
+	head = &hello;
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	new->data = strdup("Hello");
+	new->len = 5;
+	new->link = head;
+	head = new;
+	n = print_list(head);
+	printf("-> %lu elements\n", n);
+	free(new);
+	return(0);
+}
---
+++ singly_listed_lists/lists.h
@@ -0,0 +1,10 @@
+#ifndef LISTS_H
+#define LISTS_H
+int _putchar(char c);
+typedef struct list
+{
+	size_t data;
+	struct list *link;
+} list_t;
+size_t print_list(const list_h *h);
+#endif
