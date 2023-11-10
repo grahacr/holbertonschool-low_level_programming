e4abeac7d9393d776ea656bfc93d3749f52422d5
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 13:49:50 -0800

    function_pointers

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ * list_len - return number of elements in linked list
+ * @h: input string
+ * Return: count on success
+ */
+size_t list_len(const list_t *h)
+{
+	const list_t *current = h;
+	size_t count = 0;
+
+	if (current->len != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
