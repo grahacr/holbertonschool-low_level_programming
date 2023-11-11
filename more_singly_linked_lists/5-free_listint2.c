8b53f436e7f0ef136533fbb9be176423b244c06a
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 20:30:19 -0800

    function_pointers

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -8,11 +8,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	while (head != NULL)
+	while (*head != NULL)
 	{
-		temp = head;
-		head = head->next;
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
 	}
-	head = NULL;
+	*head = NULL;
 }
