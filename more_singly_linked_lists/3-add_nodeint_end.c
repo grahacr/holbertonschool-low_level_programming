b73c750e23ffb3adb2892dd0113e8077f82590b8
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 19:55:39 -0800

    function_pointers

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *temp = (listint_t *)malloc(sizeof(listint_t));
+
+	if (temp == NULL)
+	{
+		return (0);
+	}
+	temp->n = n;
+	temp->next == NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+	}
+	else
+	{
+		listint_t *current = *head;
+	
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = temp;
+	}
+	return (temp);
+}
