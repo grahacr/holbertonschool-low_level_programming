5194bf90c2658ab4a81af387e4395dbbbf27fe35
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 21:07:36 -0800

    function_pointers

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -7,13 +7,13 @@
  */
 int pop_listint(listint_t **head)
 {
+	listint_t *temp = *head;
+	size_t data = temp->n;
+
 	if (*head == NULL || head == NULL)
 	{
 		return (0);
 	}
-	listint_t *temp = *head;
-	size_t data = temp->n;
-
 	*head = temp->next;
 	free(temp);
 	return (data);
