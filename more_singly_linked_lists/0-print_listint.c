185b36acb2ecaef04b6d8fe880e40c2678b69c40
Courtney Graham <7194@holbertonstudents.com>
2023-11-10 19:11:57 -0800

    function_pointers

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,19 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current = h;
-	size_t count = 0;
+	size_t count;
 
-	while (current != NULL)
+	if (h == NULL)
+	{
+		return (NULL);
+	}
+	listint_t *ptr = NULL;
+	ptr = h;
+	while (ptr != NULL)
 	{
 		count++;
-		current = current->next;
+		printf("%d ", ptr->((int)str));
+		ptr = ptr->next;
 	}
 	return (count);
 }
