6b6f7f08e46f67de89aca3628355bd52cfb93c82
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 15:35:06 -0800

    bit_manipulation

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,6 +10,10 @@ unsigned int binary_to_uint(const char *b)
 	int i = 0;
 	unsigned int convert = 0;
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 	while (b[i] != '\0')
 	{
 		if (b[i] == '0' || b[i] == '1')
