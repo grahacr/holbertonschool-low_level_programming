a538c65080acf82001de43840d11fdaafda9955a
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 10:11:53 -0800

    function_pointers

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -23,7 +23,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		{
 			if (count == index)
 			{
-				return (ptr);
+				return (listint_t *)((int)count);
 			}
 			count++;
 			ptr = ptr->next;
