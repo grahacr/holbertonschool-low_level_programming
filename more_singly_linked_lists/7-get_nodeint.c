236ff8b937676e602148f8ac5a22860a7ce836c4
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 09:47:41 -0800

    function_pointers

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * got_nodeint_at_index - print desired index
+ * @head: head of singly linked list for traversing
+ * @index: desired index
+ * Return: index on success, null on failure
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	if (head == NULL || n < 1)
+	{
+		return (NULL)
+	}
+	unsigned int count = 0;
+	listint_t *ptr = head;
+
+	while (ptr != NULL)
+	{
+		if (count == index)
+		{
+			return (ptr);
+		}
+		count++;
+		ptr = ptr->next;
+	}
+	return (NULL);
+}
