17dd339caf767aaae7f02c3dcb71bc47f53235a3
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 11:57:13 -0800

    function_pointers

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -6,15 +6,10 @@ int sum_listint(listint_t *head)
 	int sum = 0;
 	listint_t *ptr = head;
 
-	if (ptr == NULL)
-	{
-		return (0);
-	}
-	else
+	while (ptr != NULL)
 	{
 		sum = sum + ptr->n;
 		ptr = ptr->next;
 	}
 	return (sum);
 }
-
