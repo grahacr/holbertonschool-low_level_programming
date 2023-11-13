fd77e6c32ff386bb807d5fcd803046b963ddfb43
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 11:58:39 -0800

    function_pointers

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * sum_listint - sum integer data in singly linked list
+ * @head: head of list to use to traverse
+ * Return: sum of integer data on success, 0 if empty
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
