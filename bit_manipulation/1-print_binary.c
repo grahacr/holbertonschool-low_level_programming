21101fb6595a73e7b7571938eae54ac612f8629d
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 15:48:48 -0800

    bit_manipulation

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ */
+void print_binary(unsigned long int n)
+{
+	int count = sizeof(n) * 8;
+	int i;
+
+	for (i = count - 1; i >= 0; i--)
+	{
+		int bit = (n >> i) & 1;
+		printf("%d", bit);
+	}
+}	
+
