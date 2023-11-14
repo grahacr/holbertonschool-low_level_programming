f7d45e0491df5f030cf802ff5882ae78916589d2
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 20:46:33 -0800

    bit_manipulation

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -6,12 +6,15 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int mask = 1 << index;
+	unsigned long int mask = 1 << index;
 	unsigned long int bits = sizeof(unsigned long) * 8;
 
 	if (index >= bits)
 	{
 		return (-1);
 	}
-	return n | mask;
+	else
+	{
+		return (n | mask)
+	}
 }
