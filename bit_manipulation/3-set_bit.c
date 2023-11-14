c2c97bd9be4e06cef692be2d875add64a7d089a8
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 20:58:23 -0800

    bit_manipulation

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * set_bit - set bit at certain index
+ * @n: integer input
+ * @index: index integer to set bit
+ * Return: 1 on success, -1 on failure
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
