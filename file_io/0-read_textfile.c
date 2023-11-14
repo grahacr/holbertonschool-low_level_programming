f32c4461e95581bc471bbd2dbb52e9ebc753d6a2
Courtney Graham <7194@holbertonstudents.com>
2023-11-13 22:08:45 -0800

    bit_manipulation

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <fcntl.h>
+#include <stddef.h>
 #include <unistd.h>
 #include <stdio.h>
 /**
@@ -9,12 +10,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int descriptor = open(filename, 0_RDONLY);
 	char buff[letters];
+	ssize_t bread = read(descriptor, buff, sizeof(buff));
+
 	if (descriptor == -1)
 	{
 		return (-1);
 	}
-	ssize_t bread = read(descriptor, buff, sizeof(buff));
-
 	if (bread == -1)
 	{
 		return (-1);
