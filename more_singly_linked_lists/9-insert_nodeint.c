77434b1246cc180d1d4feddfff3becced7c12329
Courtney Graham <7194@holbertonstudents.com>
2023-11-14 21:57:48 -0800

    bit_manipulation

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -8,7 +8,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr = malloc(sizeof(listint_t));
 	listint_t *current;
-	int i;
+	unsigned int i;
 
 	if (head == NULL || ptr == NULL)
 	{
