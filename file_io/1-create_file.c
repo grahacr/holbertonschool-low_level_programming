5a6415c4e6fb782869f9e3458e645fc4fd6f2ee9
Courtney Graham <7194@holbertonstudents.com>
2023-11-14 21:02:21 -0800

    bit_manipulation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -6,8 +6,10 @@
 #include <unistd.h>
 #include <stddef.h>
 /**
- *
- *
+ * create_file - create file using system calls
+ * @filename: name of file to create
+ * @text_content: string of content to write to file
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
