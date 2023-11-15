4cc63f79ffcce8893b41d37c1757a55a6d81b280
Courtney Graham <7194@holbertonstudents.com>
2023-11-14 21:11:13 -0800

    bit_manipulation

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stddef.h>
+/**
+ * append_text_to_file - append text to end of a file
+ * @filename: file to append to end of
+ * @text content: text to append to end of file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int op, wr;
+
+	if (filename == NULL || text_content == NULL)
+	{
+		return (-1);
+	}
+	op = open(filename, O_WRONLY | O_APPEND);
+	if (op == -1)
+	{
+		return (-1);
+	}
+	wr = write(op, text_content, strlen(text_content));
+	if (wr == -1)
+	{
+		close(op);
+		return (-1);
+	}
+	if (close(op) == -1)
+	{
+		return (-1);
+	}
+	return (1);
+}
