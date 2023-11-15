9c176e22a04bb677b03f7f2437a5af867cee93ba
Courtney Graham <7194@holbertonstudents.com>
2023-11-14 20:38:25 -0800

    bit_manipulation

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,13 +5,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * read_textfile - read text file and write output to stdout
+ * @filename: name of file to open and read
+ * @letters: amount of letters in the file
+ * Return: rd on success, 0 on failure
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t op, rd, wr;
 	char *buff;
-	buff = malloc(sizeof(char)*letters);
+
+	buff = malloc(sizeof(char)* letters);
 
 	if (buff == NULL || filename == NULL)
 	{
@@ -23,8 +27,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (op == -1 || rd == -1 || wr == -1)
 	{
-		free(buff);
-		return(0);
+		free (buff);
+		return (0);
 	}
 	free(buff);
 	close(op);
