c15b2fdd76edba3a3d549e048c4246ef2b8400e0
Courtney Graham <7194@holbertonstudents.com>
2023-11-15 10:28:50 -0800

    bit_manipulation

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -19,7 +19,8 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = temp;
-		ptr->prev = NULL;
+		temp->prev = NULL;
+	}
 	else
 	{
 		dlistint_t *ptr = *head;
