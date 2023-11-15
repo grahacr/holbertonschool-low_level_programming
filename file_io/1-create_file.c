079cd990a40457a51f9c5b796d4260525a04b671
Courtney Graham <7194@holbertonstudents.com>
2023-11-14 21:01:11 -0800

    bit_manipulation

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC);
+	op = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (op == -1)
 	{
 		return (-1);
