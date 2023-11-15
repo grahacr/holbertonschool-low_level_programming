b851273371d7ee4b9fa2d015302560b0cb1e54c1
Courtney Graham <7194@holbertonstudents.com>
2023-11-15 11:25:14 -0800

    bit_manipulation

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -15,19 +15,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 	new->n = n;
-
 	if (idx == 0)
 	{
 		new->prev = NULL;
 		new->next = *h;
 	if (*h != NULL)
-	{
 		(*h)->prev = new;
-	}
 	*h = new;
 	return (new);
 	}
