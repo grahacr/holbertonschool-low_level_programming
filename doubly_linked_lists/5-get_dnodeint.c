f94a3a07dad0c7dec1608bdce62795d5b5ac798b
Courtney Graham <7194@holbertonstudents.com>
2023-11-15 10:48:26 -0800

    bit_manipulation

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *
+ * get_dnodeint_at_index - get node at index of doubly linked list
+ * @head: pointer to head of list
+ * @index: index at which node is to be returned
+ * Return: NULL
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
@@ -17,7 +20,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 			return (ptr);
 		}
 		count++;
-		ptr=ptr->next;
+		ptr = ptr->next;
 	}
 	return (NULL);
 }
