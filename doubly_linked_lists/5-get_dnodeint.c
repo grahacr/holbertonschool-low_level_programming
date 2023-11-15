1dad786a93b3f3a86ec2678b16696128a140867c
Courtney Graham <7194@holbertonstudents.com>
2023-11-15 10:45:05 -0800

    bit_manipulation

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "list.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	dlistint_t *ptr;
+
+	ptr = head;
+	while (ptr != NULL)
+	{
+		if (count == index)
+		{
+			return (ptr);
+		}
+		count++;
+		ptr=ptr->next;
+	}
+	return (NULL);
+}
