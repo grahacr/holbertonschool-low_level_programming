ad30dae2321a80f5542a3407f6d45605851cc884
Courtney Graham <7194@holbertonstudents.com>
2023-11-17 10:06:37 -0800

    bit_manipulation

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
+ * hash_table_create - create hash table
+ * @size: size of table
+ * Return: hash table on success, NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t* ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht = malloc(sizeof(hash_table_t));
 
 	if (ht == NULL)
 	{
 		return (NULL);
 	}
-	ht->array = malloc(size * sizeof(hash_table_t*));
+	ht->array = malloc(size * sizeof(hash_table_t *));
 	if (ht->array == NULL)
 	{
 		free(ht);
