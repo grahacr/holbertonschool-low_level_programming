1a7d1dcb434ebcfd2ccd6e8508b6f91b2befbdaa
Courtney Graham <7194@holbertonstudents.com>
2023-11-20 14:36:08 -0800

    bit_manipulation

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	int index = hash_djb2(key);
+	hash_node_t *new = malloc(sizeof(hash_node_t));
+
+	strcpy(new->key, key);
+	new->value = value;
+	new->next = NULL;
+
+	if (ht[index] == NULL)
+		ht[index] = new;
+	else
+	{
+		hash_node_t *current = ht[index];
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new;
+	}
+}
