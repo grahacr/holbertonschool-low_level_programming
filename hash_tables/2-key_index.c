77e5b8e7b65e51fdea6b6da87feab0d20c3e1df2
Courtney Graham <7194@holbertonstudents.com>
2023-11-20 12:26:24 -0800

    bit_manipulation

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -7,5 +7,10 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2((unsigned char *)key, size));
+	int i;
+
+	if (i = 0; i < size; i++)
+	{
+		return (hash_djb2((unsigned char *)key));
+	}
 }
