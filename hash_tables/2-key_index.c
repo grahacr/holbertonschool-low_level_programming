eae8ac13f28bf5b26825a7e17bba909a784380e0
Courtney Graham <7194@holbertonstudents.com>
2023-11-20 12:53:50 -0800

    bit_manipulation

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *
- *
+ * key_index - find index of a key
+ * key: key
+ * @size: size of array of hash table
+ * Return: index on success
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
