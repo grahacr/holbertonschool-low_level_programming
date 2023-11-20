0ed2769a19154a52cecbd9b47d56a1305aeeba47
Courtney Graham <7194@holbertonstudents.com>
2023-11-20 12:24:36 -0800

    bit_manipulation

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -7,17 +7,5 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = hash_djb2((const unsigned char *)key);
-	int j;
-
-	while (hash != NULL)
-	{
-		for (j = 0; j < size, j++)
-		{
-			if (strcmp(hash[j]->key, key) == 0)
-			{
-				return hash[h]->index;
-			}
-		}
-	}
-}		
+	return (hash_djb2((unsigned char *)key, size));
+}
