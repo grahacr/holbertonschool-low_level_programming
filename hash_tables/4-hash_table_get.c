3eda20f07dbf242713ba0bf8d0c377b1b25ed68d
Courtney Graham <7194@holbertonstudents.com>
2023-11-21 16:43:59 -0800

    bit_manipulation

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -15,10 +15,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(current->key, key) == 0)
 		{
-			return current->value;
+			return (current->value);
 			current = current->next;
 		}
-		return NULL;
 	}
-	return (current->value);
+	return (NULL);
 }
