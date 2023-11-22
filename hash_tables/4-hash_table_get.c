fc80b03875afd903ff886275bbda0ae11aff7a9c
Courtney Graham <7194@holbertonstudents.com>
2023-11-21 16:39:58 -0800

    bit_manipulation

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *
+ *
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index = hash_djb2((const unsigned char *)key) % ht->size;
+	hash_node_t *current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(current->key, key) == 0)
+		{
+			return current->value;
+		}
+		current = current->next;
+	}
+	return NULL;
+}
