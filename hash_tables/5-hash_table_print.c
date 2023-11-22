8e87a6c5c599bd6f18a26dd69006d40aea2e1d4d
Courtney Graham <7194@holbertonstudents.com>
2023-11-21 20:43:44 -0800

    bit_manipulation

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -9,17 +9,19 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	long unsigned int i;
+	int flag = 0;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i])
 		{
-			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
-			if (i < ht->size - 1 && ht->array[i + 1])
+			if (flag)
 			{
 				printf(", ");
 			}
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			flag = 1;
 		}
 	}
 	printf("}");
