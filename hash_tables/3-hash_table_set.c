f68f428113cddda94d41ef6f28006eeb82e660ad
Courtney Graham <7194@holbertonstudents.com>
2023-11-22 20:31:37 -0800

    bit_manipulation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -6,7 +6,7 @@
  * hash_table_set - add element to hash table
  * @ht: hash table to add value or update value to
  * @key: key. cannot be empty
- * @value: associated with key. must be duplicated. 
+ * @value: associated with key. must be duplicated.
  * Return: 1 on success, 0 on failure
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
@@ -33,6 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	else
 	{
 		hash_node_t *current = ht->array[index];
+
 		while (current != NULL)
 		{
 			if (strcmp(current->key, key) == 0)
@@ -45,9 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (1);
 			}
 			if (current->next == NULL)
-			{
 				break;
-			}
 		current = current->next;
 		}
 	new->next = ht->array[index];
