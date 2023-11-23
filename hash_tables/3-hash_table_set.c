68315f90033c6fddb47846cdc3fadff44030b4ed
Courtney Graham <7194@holbertonstudents.com>
2023-11-22 20:35:37 -0800

    bit_manipulation

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,8 +14,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int index = hash_djb2((const unsigned char *)key) % ht->size;
 	hash_node_t *new = malloc(sizeof(hash_node_t));
 
+	if (new == NULL)
+		return (0);
 	new->key = strdup(key);
-	if (new->key == NULL || new == NULL)
+	if (new->key == NULL)
 	{
 		free(new);
 		return (0);
