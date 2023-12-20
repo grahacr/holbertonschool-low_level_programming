813c7d78d54aceca91b7ad4cb1de99fd8e05c1e9
Courtney Graham <7194@holbertonstudents.com>
2023-12-20 14:26:05 -0800

    trying to fix print

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -20,13 +20,13 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
-			printf("%d", array[i]);
-			if (i < high)
-				printf(", ");
+			printf("%d", array[i])'
+				if (i < high)
+					printf(", ");
 		}
 		printf("\n");
 		if (array[mid] == value)
-			return(mid);
+			return (mid);
 		if (array[mid] < value)
 			low = mid + 1;
 		else
