e72e1658466baca1dd53fd0ae65e188bb672777a
Courtney Graham <7194@holbertonstudents.com>
2023-12-20 14:27:06 -0800

    fix typo

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -20,7 +20,7 @@ int binary_search(int *array, size_t size, int value)
 		printf("Searching in array: ");
 		for (i = low; i <= high; i++)
 		{
-			printf("%d", array[i])'
+			printf("%d", array[i]);
 				if (i < high)
 					printf(", ");
 		}
